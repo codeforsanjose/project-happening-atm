60edf52c7d810772fa676ebca1e0a8627a5e2c24
Trace <teohrt18@gmail.com>
2020-09-26 22:00:07 -0700

    Serve frontend

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require("path");
 const express = require("express");
 const apiResponse = require("./utilities/apiResponse");
 const graphqlMiddleware = require("./graphql/graphql");
@@ -10,12 +11,18 @@ const dbClient = require("./db/dbClient")(logger);
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "..", "client/build")));
+app.use(express.static("client/public"));
 app.use(express.json());
 
 app.use( (req, res, done) => {
     logger.info('Request URL: ' + req.originalUrl);
     logger.info('Request body: ' + JSON.stringify(req.body));
     done();
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
 app.use('/graphql/', graphqlMiddleware(dbClient, twilioClient, logger));
