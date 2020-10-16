4205659815a96d4ddb90a2f90e26b566f406f1e9
Terry Chiem <terrychiem@gmail.com>
2020-10-16 13:56:57 -0700

    Add placeholder for AdminView

---
+++ src/components/AdminView/AdminView.jsx
@@ -0,0 +1,13 @@
+import React, { useCallback, useState } from 'react';
+import './AdminView.scss';
+
+function AdminView() {
+
+    return (
+        <div className="admin-view">
+            I am the Admin View
+        </div>
+    );
+}
+
+export default AdminView;
