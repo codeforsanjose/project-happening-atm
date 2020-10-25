6f622cc271d1ad2c70b8b2de67f5339c6f8422a3
Trace <teohrt18@gmail.com>
2020-10-25 12:11:24 -0700

    Modified heroku server

--- backend/heroku-server.js
+++ backend/heroku-server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const app = express();
-app.use(express.static(__dirname + '../frontend/'));
+app.use(express.static(`${__dirname}../frontend/`));
 app.listen(process.env.PORT || 8080);
