a6d01a860dbb63a17ffe529985aaaa48039f54a8
Terry Chiem <terrychiem@gmail.com>
2020-10-26 12:25:24 -0700

    Add file input to the csv upload view

--- src/components/AdminView/AdminViewPages/AdminUploadView/AdminUploadView.jsx
+++ src/components/AdminView/AdminViewPages/AdminUploadView/AdminUploadView.jsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './AdminUploadView.scss';
 
 import { ReactComponent as PublishIcon } from '../../../../assets/publish-24px.svg';
 import DragAndDrop from './DragAndDrop';
 
 function AdminUploadView() {
+  const fileInputRef = useRef();
+
+  function handleFileChange(e) {
+    const fileRef = fileInputRef.current;
+    console.log('file is:', fileRef.files);
+  }
+
   return (
     <div className="admin-upload">
       <DragAndDrop>
-        <div id="upload-area">
+        <div className="upload-area">
           <PublishIcon />
           <p>Drag and Drop CSV File</p>
-          <p>Or Upload from your Computer</p>
+          <label for="csv">
+            Or Upload from your Computer
+          </label>
+          <input
+            className="visually-hidden"
+            type="file"
+            name="csv"
+            id="csv"
+            accept=".csv"
+            onChange={handleFileChange}
+            ref={fileInputRef}
+          />
         </div>
       </DragAndDrop>
     </div>
