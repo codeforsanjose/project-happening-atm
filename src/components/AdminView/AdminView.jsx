04ecbc1c4df5e0d1bfc43d9f60456b9cc1f6c467
Ian Chu <hien.chu@icloud.com>
2020-11-12 20:04:34 -0800

    Rough styles and functionality

--- src/components/AdminView/AdminView.jsx
+++ src/components/AdminView/AdminView.jsx
@@ -4,19 +4,21 @@ import AdminNavigation from './AdminNavigation'
 import AgendaTable from '../AgendaTable/AgendaTable'
 
 function AdminView() {
-    return (
-        <div className="admin-view">
-          <AdminNavigation />
-          <div className="admin-view-content">
-            <div className="admin-view-forehead">
-              <h3>Agenda for Tiw's Day,  Septemo-membris Unus</h3>
-            </div>
-            <h3 id="header">Edit Agenda Items</h3>
-            <hr/>
-            <AgendaTable />
-          </div>
+  return (
+    <div className="admin-view">
+      <AdminNavigation />
+      <div className="admin-view-content">
+        <div className="admin-view-forehead">
+          <h3>Agenda for Tiw's Day, Septemo-membris Unus</h3>
         </div>
-    );
+        <div className="admin-view-header">
+          <h3 id="header">Edit Agenda Items</h3>
+          <hr></hr>
+        </div>
+        <AgendaTable />
+      </div>
+    </div>
+  )
 }
 
 export default AdminView
