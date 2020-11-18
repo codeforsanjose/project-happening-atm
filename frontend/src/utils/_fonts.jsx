60addfd1326d430026329e0afac36071e68aa264
sepsol <SR-75@live.com>
2020-11-18 15:13:15 -0800

    added a fonts util helper component

---
+++ frontend/src/utils/_fonts.jsx
@@ -0,0 +1,57 @@
+import { ReactComponent as AddIcon } from '../assets/icons/add-24px.svg';
+import { ReactComponent as ArrowUpwardIcon } from '../assets/icons/arrow_upward-24px.svg';
+import { ReactComponent as CalendarTodayIcon } from '../assets/icons/calendar_today-24px.svg';
+import { ReactComponent as CallIcon } from '../assets/icons/call-24px.svg';
+import { ReactComponent as CancelIcon } from '../assets/icons/cancel-24px.svg';
+import { ReactComponent as CheckedCheckboxIcon } from '../assets/icons/check_box-24px.svg';
+import { ReactComponent as UncheckedCheckboxIcon } from '../assets/icons/check_box_outline_blank-24px.svg';
+import { ReactComponent as CloseIcon } from '../assets/icons/close-24px.svg';
+import { ReactComponent as DeleteIcon } from '../assets/icons/delete-24px.svg';
+import { ReactComponent as DoneOutlineIcon } from '../assets/icons/done_outline-24px.svg';
+import { ReactComponent as EditIcon } from '../assets/icons/edit-24px.svg';
+import { ReactComponent as EmailIcon } from '../assets/icons/email-24px.svg';
+import { ReactComponent as ImportExportIcon } from '../assets/icons/import_export-24px.svg';
+import { ReactComponent as InfoIcon } from '../assets/icons/info-24px.svg';
+import { ReactComponent as KeyboardArrowDownIcon } from '../assets/icons/keyboard_arrow_down-24px.svg';
+import { ReactComponent as KeyboardArrowUpIcon } from '../assets/icons/keyboard_arrow_up-24px.svg';
+import { ReactComponent as LibraryAddCheckIcon } from '../assets/icons/library_add_check-24px.svg';
+import { ReactComponent as MenuIcon } from '../assets/icons/menu-24px.svg';
+import { ReactComponent as NewReleasesIcon } from '../assets/icons/new_releases-24px.svg';
+import { ReactComponent as NotificationsIcon } from '../assets/icons/notifications-24px.svg';
+import { ReactComponent as OnDemandVideoIcon } from '../assets/icons/ondemand_video-24px.svg';
+import { ReactComponent as PublishIcon } from '../assets/icons/publish-24px.svg';
+import { ReactComponent as RemoveIcon } from '../assets/icons/remove-24px.svg';
+import { ReactComponent as ScheduleIcon } from '../assets/icons/schedule-24px.svg';
+import { ReactComponent as SearchIcon } from '../assets/icons/search-24px.svg';
+import { ReactComponent as ShareIcon } from '../assets/icons/share-24px.svg';
+import { ReactComponent as VoiceChatIcon } from '../assets/icons/voice_chat-24px.svg';
+
+export {
+  AddIcon,
+  ArrowUpwardIcon,
+  CalendarTodayIcon,
+  CallIcon,
+  CancelIcon,
+  CheckedCheckboxIcon,
+  UncheckedCheckboxIcon,
+  CloseIcon,
+  DeleteIcon,
+  DoneOutlineIcon,
+  EditIcon,
+  EmailIcon,
+  ImportExportIcon,
+  InfoIcon,
+  KeyboardArrowDownIcon,
+  KeyboardArrowUpIcon,
+  LibraryAddCheckIcon,
+  MenuIcon,
+  NewReleasesIcon,
+  NotificationsIcon,
+  OnDemandVideoIcon,
+  PublishIcon,
+  RemoveIcon,
+  ScheduleIcon,
+  SearchIcon,
+  ShareIcon,
+  VoiceChatIcon
+};
