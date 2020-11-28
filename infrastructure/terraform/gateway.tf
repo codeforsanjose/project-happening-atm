690166b34a52e4908ce077e6ce5c9b70b2137d4f
Trace <teohrt18@gmail.com>
2020-11-27 22:17:17 -0800

    Add new lambda to Terraform scripts - log group & api gateway included

--- infrastructure/terraform/gateway.tf
+++ infrastructure/terraform/gateway.tf
@@ -1,27 +1,30 @@
-resource "aws_api_gateway_rest_api" "gan_graphql_api" {
-  name        = "gan_graphql_api-${terraform.workspace}-gateway"
+resource "aws_api_gateway_rest_api" "gan" {
+  name        = "gan_api-${terraform.workspace}-gateway"
   description = "This is my API for demonstration purposes"
 }
 
 resource "aws_api_gateway_deployment" "dev" {
-  rest_api_id = "${aws_api_gateway_rest_api.gan_graphql_api.id}"
+  rest_api_id = "${aws_api_gateway_rest_api.gan.id}"
   stage_name  = "dev"
 
   depends_on = [
-    "aws_api_gateway_integration.gan",
+    "aws_api_gateway_integration.gan_graphql_api",
   ]
 }
 
-resource "aws_api_gateway_resource" "gan_graphql_api_proxy" {
-  parent_id   = "${aws_api_gateway_rest_api.gan_graphql_api.root_resource_id}"
-  path_part   = "{proxy+}"
-  rest_api_id = "${aws_api_gateway_rest_api.gan_graphql_api.id}"
+
+# GraphQL API route and lambda intergration
+# -----------------------------------------
+resource "aws_api_gateway_resource" "gan_graphql_api" {
+  parent_id   = "${aws_api_gateway_rest_api.gan.root_resource_id}"
+  path_part   = "agendapi"
+  rest_api_id = "${aws_api_gateway_rest_api.gan.id}"
 }
 
-resource "aws_api_gateway_method" "gan" {
+resource "aws_api_gateway_method" "gan_graphql_api" {
   http_method      = "ANY"
-  resource_id      = "${aws_api_gateway_resource.gan_graphql_api_proxy.id}"
-  rest_api_id      = "${aws_api_gateway_rest_api.gan_graphql_api.id}"
+  resource_id      = "${aws_api_gateway_resource.gan_graphql_api.id}"
+  rest_api_id      = "${aws_api_gateway_rest_api.gan.id}"
   authorization    = "NONE"
   api_key_required = false
 
@@ -30,22 +33,63 @@ resource "aws_api_gateway_method" "gan" {
   }
 }
 
-resource "aws_api_gateway_integration" "gan" {
-  http_method             = "${aws_api_gateway_method.gan.http_method}"
-  resource_id             = "${aws_api_gateway_resource.gan_graphql_api_proxy.id}"
-  rest_api_id             = "${aws_api_gateway_rest_api.gan_graphql_api.id}"
+resource "aws_api_gateway_integration" "gan_graphql_api" {
+  http_method             = "${aws_api_gateway_method.gan_graphql_api.http_method}"
+  resource_id             = "${aws_api_gateway_resource.gan_graphql_api.id}"
+  rest_api_id             = "${aws_api_gateway_rest_api.gan.id}"
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "${aws_lambda_function.gan_graphql_lambda.invoke_arn}"
 
   depends_on = [
-    "aws_api_gateway_method.gan",
+    "aws_api_gateway_method.gan_graphql_api",
   ]
 }
 
-resource "aws_lambda_permission" "apigateway_lambda_invoke" {
+resource "aws_lambda_permission" "invoke_graphql_api" {
   action        = "lambda:InvokeFunction"
   function_name = "${aws_lambda_function.gan_graphql_lambda.arn}"
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_deployment.dev.execution_arn}/*"
+}
+
+
+# Agenda Upload service route and lambda intergration
+# ---------------------------------------------------
+resource "aws_api_gateway_resource" "gan_agenda_upload" {
+  parent_id   = "${aws_api_gateway_rest_api.gan.root_resource_id}"
+  path_part   = "upload"
+  rest_api_id = "${aws_api_gateway_rest_api.gan.id}"
+}
+
+resource "aws_api_gateway_method" "gan_agenda_upload" {
+  http_method      = "POST"
+  resource_id      = "${aws_api_gateway_resource.gan_agenda_upload.id}"
+  rest_api_id      = "${aws_api_gateway_rest_api.gan.id}"
+  authorization    = "NONE"
+  api_key_required = false
+
+  request_parameters = {
+    "method.request.path.proxy" = true
+  }
+}
+
+resource "aws_api_gateway_integration" "gan_agenda_upload" {
+  http_method             = "${aws_api_gateway_method.gan_agenda_upload.http_method}"
+  resource_id             = "${aws_api_gateway_resource.gan_agenda_upload.id}"
+  rest_api_id             = "${aws_api_gateway_rest_api.gan.id}"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.gan_agenda_upload_lambda.invoke_arn}"
+
+  depends_on = [
+    "aws_api_gateway_method.gan_agenda_upload",
+  ]
+}
+
+resource "aws_lambda_permission" "invoke_agenda_upload" {
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.gan_agenda_upload_lambda.arn}"
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_api_gateway_deployment.dev.execution_arn}/*"
 }
--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_lambda_function" "gan_graphql_lambda" {
   depends_on = [
     aws_db_instance.gan_db,
-    aws_cloudwatch_log_group.gan_lambda_log_group,
+    aws_cloudwatch_log_group.gan_graphql_lambda_log_group,
     aws_security_group.demosg,
     aws_subnet.demo_subnet1,
     aws_subnet.demo_subnet2,
@@ -42,8 +42,47 @@ resource "aws_lambda_function" "gan_graphql_lambda" {
   }
 }
 
-resource "aws_cloudwatch_log_group" "gan_lambda_log_group" {
+resource "aws_cloudwatch_log_group" "gan_graphql_lambda_log_group" {
   name              = "/aws/lambda/gan_graphql_lambda"
+  retention_in_days = 14
+}
+
+resource "aws_lambda_function" "gan_agenda_upload_lambda" {
+  depends_on = [
+    aws_db_instance.gan_db,
+    aws_cloudwatch_log_group.gan_agenda_upload_lambda_log_group,
+    aws_security_group.demosg,
+    aws_subnet.demo_subnet1,
+    aws_subnet.demo_subnet2,
+    aws_subnet.demo_subnet3,
+  ]
+
+  function_name    = "gan_agenda_upload_lambda"
+  handler          = "handler.uploadHandler"
+  filename         = "../gan_agenda_upload_lambda.zip"
+  source_code_hash = filebase64sha256("../gan_agenda_upload_lambda.zip")
+  runtime          = "nodejs12.x"
+  role             = "${aws_iam_role.gan_graphql_lambda.arn}"
+
+  vpc_config {
+    subnet_ids         = ["${aws_subnet.demo_subnet1.id}", "${aws_subnet.demo_subnet2.id}", "${aws_subnet.demo_subnet3.id}"]
+    security_group_ids = "${list("${aws_security_group.demosg.id}")}"
+  }
+
+  environment {
+    variables = {
+      IS_LAMBDA  = true
+      PGUSER     = "${var.PGUSER}"
+      PGPASSWORD = "${var.PGPASSWORD}"
+      PGDATABASE = "${var.PGDATABASE}"
+      PGHOST     = "${aws_db_instance.gan_db.address}"
+      PGPORT     = "${aws_db_instance.gan_db.port}"
+    }
+  }
+}
+
+resource "aws_cloudwatch_log_group" "gan_agenda_upload_lambda_log_group" {
+  name              = "/aws/lambda/gan_agenda_upload_lambda"
   retention_in_days = 14
 }
 
