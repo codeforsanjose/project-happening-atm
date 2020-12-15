a44b8d60652be2d79bf0e086cd771daa0ffe945c
Terry Chiem <terrychiem@gmail.com>
2020-12-15 13:07:15 -0800

    Fix ESLint errors from merge

--- frontend/src/components/MeetingView/MeetingView.jsx
+++ frontend/src/components/MeetingView/MeetingView.jsx
@@ -1,40 +1,43 @@
 import React, { useState } from 'react';
 import {
-  Accordion
+  Accordion,
 } from 'react-accessible-accordion';
 import './MeetingView.scss';
 
 import { CheckedCheckboxIcon, UncheckedCheckboxIcon } from '../../utils/_icons';
 import MeetingAgendaGroup from './MeetingAgendaGroup';
 
-const TEST_ITEMS = [1,2,3,4,5].map(makeTestItem);
-
-function makeTestItem(index) {
-  const status = index <= 2 ? 'Completed' : index === 3 ? 'In Progress' : 'Pending';
-  return {
-    title: `${index} Agenda Group`,
-    description: '',
-    status,
-    items: [1,2,3,4].map((i) => makeTestSubItem(index, i, status))
-  }
-}
-
 function makeTestSubItem(parentIndex, index, status) {
   return {
+    id: `${parentIndex}-${index}`,
     title: `${parentIndex}.${index} Agenda Item`,
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel nisl euismod, tristique leo sit amet, eleifend enim.',
     status,
-    items: []
-  }
+    items: [],
+  };
 }
 
-function MeetingView() {
-  const [ items, setItems ] = useState(TEST_ITEMS);
-  const [ showCompleted, setShowCompleted ] = useState(true);
+function makeTestItem(index) {
+  // eslint-disable-next-line no-nested-ternary
+  const status = index <= 2 ? 'Completed' : index === 3 ? 'In Progress' : 'Pending';
+  return {
+    id: index,
+    title: `${index} Agenda Group`,
+    description: '',
+    status,
+    items: [1, 2, 3, 4].map((i) => makeTestSubItem(index, i, status)),
+  };
+}
 
-  const renderedItems = showCompleted ?
-    items :
-    items.filter(item => item.status !== 'Completed');
+const TEST_ITEMS = [1, 2, 3, 4, 5].map(makeTestItem);
+
+function MeetingView() {
+  const [items] = useState(TEST_ITEMS);
+  const [showCompleted, setShowCompleted] = useState(true);
+
+  const renderedItems = showCompleted
+    ? items
+    : items.filter((item) => item.status !== 'Completed');
 
   return (
     <div className="meeting-view">
@@ -42,17 +45,18 @@ function MeetingView() {
         <h3>Agenda</h3>
       </div>
 
-      <div
+      <button
+        type="button"
         className="complete-toggle"
-        onClick={() => setShowCompleted(completed => !completed)}
+        onClick={() => setShowCompleted((completed) => !completed)}
       >
         {showCompleted ? <CheckedCheckboxIcon /> : <UncheckedCheckboxIcon />}
         <p>Show Completed Items</p>
-      </div>
+      </button>
 
       <Accordion allowZeroExpanded allowMultipleExpanded className="agenda">
-        {renderedItems.map((agendaGroup,i) => (
-            <MeetingAgendaGroup key={i} agendaGroup={agendaGroup} />
+        {renderedItems.map((agendaGroup) => (
+          <MeetingAgendaGroup key={agendaGroup.id} agendaGroup={agendaGroup} />
         ))}
       </Accordion>
     </div>
