90cf13ae28a580fe70a6170c3fea33f69ff0d749
Terry Chiem <terrychiem@gmail.com>
2021-01-17 18:26:41 -0800

    Remove extra div from participate pages

--- frontend/src/components/ParticipatePages/ParticipateJoin.jsx
+++ frontend/src/components/ParticipatePages/ParticipateJoin.jsx
@@ -4,58 +4,56 @@ import { Link } from 'react-router-dom';
 
 function ParticipateJoin() {
   return (
-    <div className="ParticipatePage">
-      <div className="ParticipateContent">
-        <h3 className="ParticipateHeader">Join the Virtual Meeting</h3>
+    <div className="ParticipateContent">
+      <h3 className="ParticipateHeader">Join the Virtual Meeting</h3>
 
-        <p>
-          To help stop the spread of COVID-19 and keep our
-          community safe, the City of San José is currently
-          conducting City Council meetings virtually using
-          <span className="bold"> Zoom</span>
-          .
-        </p>
+      <p>
+        To help stop the spread of COVID-19 and keep our
+        community safe, the City of San José is currently
+        conducting City Council meetings virtually using
+        <span className="bold"> Zoom</span>
+        .
+      </p>
 
-        <p>
-          You have 2 options to join and provide spoken
-          comment in the virtual meeting:
-        </p>
+      <p>
+        You have 2 options to join and provide spoken
+        comment in the virtual meeting:
+      </p>
 
-        <ol>
-          <li>
-            <p>Call ###-###-####</p>
-            <p className="info">
-              Enter Meeting ID ### #### ####
-              Select *9 to &#34;Raise Hand&#34; to speak.
-              Select *6 to unmute when your name is called.
-            </p>
-          </li>
-          <li>
-            <p>Join from your computer or smart device here*</p>
-            <Link to="/">
-              <button type="button">Join Zoom Meeting</button>
-            </Link>
-            <p className="info bold">*Zoom Web Browser Requirements</p>
-            <p className="info">
-              Must have the application installed. Use a current,
-              up-to-date browser: Chrome 30+, Firefox 27+,
-              Microsoft Edge 12+, Safari 7+. Certain functionality
-              may be disabled in older browsers including Internet
-              Explorer. Learn more at
-              {' '}
-              <a href="#">zoom.us</a>
-              .
-            </p>
+      <ol>
+        <li>
+          <p>Call ###-###-####</p>
+          <p className="info">
+            Enter Meeting ID ### #### ####
+            Select *9 to &#34;Raise Hand&#34; to speak.
+            Select *6 to unmute when your name is called.
+          </p>
+        </li>
+        <li>
+          <p>Join from your computer or smart device here*</p>
+          <Link to="/">
+            <button type="button">Join Zoom Meeting</button>
+          </Link>
+          <p className="info bold">*Zoom Web Browser Requirements</p>
+          <p className="info">
+            Must have the application installed. Use a current,
+            up-to-date browser: Chrome 30+, Firefox 27+,
+            Microsoft Edge 12+, Safari 7+. Certain functionality
+            may be disabled in older browsers including Internet
+            Explorer. Learn more at
+            {' '}
+            <a href="#">zoom.us</a>
+            .
+          </p>
 
-            <p className="info">
-              For more information on how to join a meeting,
-              {' '}
-              <a href="#">click here</a>
-              .
-            </p>
-          </li>
-        </ol>
-      </div>
+          <p className="info">
+            For more information on how to join a meeting,
+            {' '}
+            <a href="#">click here</a>
+            .
+          </p>
+        </li>
+      </ol>
     </div>
   );
 }
--- frontend/src/components/ParticipatePages/ParticipateWatch.jsx
+++ frontend/src/components/ParticipatePages/ParticipateWatch.jsx
@@ -2,48 +2,46 @@ import React from 'react';
 
 function ParticipateWatch() {
   return (
-    <div className="ParticipatePage">
-      <div className="ParticipateContent">
-        <h3 className="ParticipateHeader">Watch Meeting Broadcast</h3>
+    <div className="ParticipateContent">
+      <h3 className="ParticipateHeader">Watch Meeting Broadcast</h3>
 
-        <p>
-          You have 3 options to watch the live meeting broadcast:
-        </p>
+      <p>
+        You have 3 options to watch the live meeting broadcast:
+      </p>
 
-        <p className="info">
-          Closed Captioning is available for all
-        </p>
+      <p className="info">
+        Closed Captioning is available for all
+      </p>
 
-        <ol>
-          <li>
-            <p>Comcast Cable Channel 26</p>
-          </li>
-          <li>
-            <p>City&apos;s YouTube Channel</p>
-            <a
-              href="https://www.youtube.com/CityOfSanJoseCalifornia"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <button type="button">Go to YouTube</button>
-            </a>
-          </li>
-          <li>
-            <p>City&apos;s Website</p>
-            <p className="info">
-              Find the meeting and click on &quot;In Progress&quot; or
-              &quot;Currently in Session&quot; to watch.
-            </p>
-            <a
-              href="https://www.sanjoseca.gov/news-stories/watch-a-meeting"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <button type="button">Go to the City&apos;s Website</button>
-            </a>
-          </li>
-        </ol>
-      </div>
+      <ol>
+        <li>
+          <p>Comcast Cable Channel 26</p>
+        </li>
+        <li>
+          <p>City&apos;s YouTube Channel</p>
+          <a
+            href="https://www.youtube.com/CityOfSanJoseCalifornia"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button type="button">Go to YouTube</button>
+          </a>
+        </li>
+        <li>
+          <p>City&apos;s Website</p>
+          <p className="info">
+            Find the meeting and click on &quot;In Progress&quot; or
+            &quot;Currently in Session&quot; to watch.
+          </p>
+          <a
+            href="https://www.sanjoseca.gov/news-stories/watch-a-meeting"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button type="button">Go to the City&apos;s Website</button>
+          </a>
+        </li>
+      </ol>
     </div>
   );
 }
