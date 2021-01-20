2c40ae1d452717758e39f18510155fe3409c706e
Ian Chu <hien.chu@icloud.com>
2021-01-20 10:34:59 -0800

    Resolving merge conflicts

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -59,15 +59,6 @@ function App() {
       <ApolloProvider client={client}>
         <div className={classnames('app-root')}>
           <Router>
-            <div className="ribbon" />
-            {/* TODO do we need it if the header is not sticky? <div className="fade-box" /> */}
-            {/*
-                        A <Switch> looks through all its children <Route>
-                        elements and renders the first one whose path
-                        matches the current URL. Use a <Switch> any time
-                        you have multiple routes, but you want only one
-                        of them to render at a time
-                        */}
             <Switch>
               <Route exact path="/">
                 <MeetingView />
