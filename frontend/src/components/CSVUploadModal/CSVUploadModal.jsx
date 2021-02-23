81b286d65cb9a7e7ba6943d914af41572cd32c16
Terry Chiem <terrychiem@gmail.com>
2021-02-22 20:20:56 -0800

    Add icon for currently selected csv file

--- frontend/src/components/CSVUploadModal/CSVUploadModal.jsx
+++ frontend/src/components/CSVUploadModal/CSVUploadModal.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-import React from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import Modal from 'react-modal';
 import './CSVUploadModal.scss';
 
@@ -15,6 +15,29 @@ const overlayStyle = {
 };
 
 function CSVUploadModal({ isOpen, closeModal }) {
+  const fileInputRef = useRef();
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  function handleFileChange() {
+    const fileRef = fileInputRef.current;
+    if (fileRef.files.length === 0) return;
+    // TODO: Validate files:
+    // https://github.com/codeforsanjose/gov-agenda-notifier/issues/32
+    setSelectedFile(fileRef.files[0]);
+  }
+
+  const handleFileDrop = useCallback((files) => {
+    if (files.length === 0) return;
+    // TODO: Validate dropped files:
+    // https://github.com/codeforsanjose/gov-agenda-notifier/issues/32
+    setSelectedFile(files[0]);
+  }, []);
+
+  function clearSelectedFile() {
+    fileInputRef.current.value = '';
+    setSelectedFile(null);
+  }
+
   return (
     <Modal
       style={{ overlay: overlayStyle }}
@@ -24,13 +47,23 @@ function CSVUploadModal({ isOpen, closeModal }) {
       className="CSVUploadModal"
     >
       <div className="wrapper">
-        <button type="button" onClick={closeModal} className="close-modal">
+        <button type="button" onClick={closeModal} className="cancel-button close-modal">
           <CancelIcon />
         </button>
         <h2>Upload New Agenda</h2>
 
-        <DragAndDrop dropHandler={() => console.log('hi')}>
+        <DragAndDrop dropHandler={handleFileDrop}>
           <div className="upload-area">
+            {selectedFile && (
+              <div className="file-preview">
+                <button type="button" onClick={clearSelectedFile} className="cancel-button cancel-file">
+                  <CancelIcon />
+                </button>
+                <div className="csv-icon">csv</div>
+                <p>{selectedFile.name}</p>
+              </div>
+            )}
+
             <PublishIcon />
             <p>Drag and Drop CSV File</p>
             <label htmlFor="csv">
@@ -41,8 +74,8 @@ function CSVUploadModal({ isOpen, closeModal }) {
                 name="csv"
                 id="csv"
                 accept=".csv"
-                onChange={() => console.log('hi')}
-                ref={() => console.log('hi')}
+                onChange={handleFileChange}
+                ref={fileInputRef}
               />
             </label>
           </div>
