8df22f17de309416f149dc3fba3faf0e237f5b0c
Ian Chu <hien.chu@icloud.com>
2021-02-23 16:47:53 -0800

    Sort sub items, merge components

--- frontend/src/components/MeetingView/AgendaView/AgendaView.jsx
+++ frontend/src/components/MeetingView/AgendaView/AgendaView.jsx
@@ -1,11 +1,24 @@
 import React, { useState } from 'react';
-import { Accordion } from 'react-accessible-accordion';
+import { Link } from 'react-router-dom';
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+} from 'react-accessible-accordion';
 import { SortableContainer, SortableElement } from 'react-sortable-hoc';
 import arrayMove from 'array-move';
 import './AgendaView.scss';
-
-import { CheckedCheckboxIcon, UncheckedCheckboxIcon } from '../../../utils/_icons';
-import AgendaItem from './AgendaItem';
+import './AgendaItem.scss';
+import './AgendaSubItem.scss';
+import {
+  CheckedCheckboxIcon,
+  UncheckedCheckboxIcon,
+  NotificationsIcon,
+  ShareIcon,
+  AddIcon,
+} from '../../../utils/_icons';
 import Search from '../../Header/Search';
 
 /**
@@ -21,24 +34,106 @@ import Search from '../../Header/Search';
  *      Boolean state to toggle if completed agenda items are shown
  */
 
+const agendaSubItemLinks = [
+  {
+    getPath: (item) => `/subscribe/${item.meetingId}/${item.id}`,
+    Icon: NotificationsIcon,
+    text: 'Subscribe',
+  },
+  {
+    getPath: (item) => '/',
+    Icon: ShareIcon,
+    text: 'Share',
+  },
+  {
+    getPath: (item) => '/',
+    Icon: AddIcon,
+    text: 'More Info',
+  },
+];
+
+function AgendaSubItem({ renderedAgendaSubItem }) {
+  return (
+    <div className="AgendaItem">
+      {renderedAgendaSubItem.status !== 'Pending' && <div className="item-status">{renderedAgendaSubItem.status}</div>}
+
+      <input type="checkbox" />
+      <h4>{renderedAgendaSubItem.title}</h4>
+      <p>{renderedAgendaSubItem.description}</p>
+
+      <div className="item-links">
+        {
+          agendaSubItemLinks.map((link) => (
+            <Link to={link.getPath(renderedAgendaSubItem)} key={link.text}>
+              <div className="link">
+                <link.Icon />
+                <p>{link.text}</p>
+              </div>
+            </Link>
+          ))
+        }
+      </div>
+    </div>
+  );
+}
+
+const SortableAgendaSubItemElement = SortableElement(
+  ({ renderedAgendaSubItem }) => (
+    <AgendaSubItem renderedAgendaSubItem={renderedAgendaSubItem} />
+  ),
+);
+
 const SortableAgendaSubItemContainer = SortableContainer(
-  ({ children }) => (
+  ({ parentIndex, renderedAgendaItem }) => (
     <Accordion allowZeroExpanded allowMultipleExpanded className="agenda">
-      {children}
+      <AccordionItem className="AgendaGroup">
+        <AccordionItemHeading className="group-header">
+          <AccordionItemButton className="group-button">
+            <div className="button-text">
+              <div className="group-title">{renderedAgendaItem.title}</div>
+              <div className="group-status">
+                {renderedAgendaItem.status === 'Pending' ? '' : renderedAgendaItem.status}
+              </div>
+            </div>
+          </AccordionItemButton>
+        </AccordionItemHeading>
+        <AccordionItemPanel className="group-items">
+          {renderedAgendaItem.subItems.map((renderedAgendaSubItem, index) => (
+            <SortableAgendaSubItemElement
+              index={index}
+              collection={parentIndex}
+              // eslint-disable-next-line react/no-array-index-key
+              key={renderedAgendaSubItem + index}
+              renderedAgendaSubItem={renderedAgendaSubItem}
+            />
+          ))}
+
+        </AccordionItemPanel>
+      </AccordionItem>
     </Accordion>
   ),
 );
 
 const SortableAgendaItemElement = SortableElement(
-  ({ renderedAgendaItem, index }) => <AgendaItem key={index} index={index} renderedAgendaItem={renderedAgendaItem} />,
+  ({ renderedAgendaItem, parentIndex, onAgendaSubItemSortEnd }) => (
+    <SortableAgendaSubItemContainer
+      parentIndex={parentIndex}
+      onSortEnd={onAgendaSubItemSortEnd}
+      renderedAgendaItem={renderedAgendaItem}
+    />
+  ),
 );
+
 const SortableAgendaItemContainer = SortableContainer(
   ({ renderedAgendaItems, onAgendaSubItemSortEnd }) => (
-    <SortableAgendaSubItemContainer onSortEnd={onAgendaSubItemSortEnd}>
-      {renderedAgendaItems.map((renderedAgendaItem, index) => (
-        <SortableAgendaItemElement renderedAgendaItem={renderedAgendaItem} index={index} />
-      ))}
-    </SortableAgendaSubItemContainer>
+    renderedAgendaItems.map((renderedAgendaItem, index) => (
+      <SortableAgendaItemElement
+        renderedAgendaItem={renderedAgendaItem}
+        index={index}
+        parentIndex={index}
+        onAgendaSubItemSortEnd={onAgendaSubItemSortEnd}
+      />
+    ))
   ),
 );
 
