92890179a99e566ca234650165192c3c61ddd986
Darren Pham <phamdarren1@gmail.com>
2021-05-13 19:08:52 -0700

    fix potential bug with graphql api - logs seem to indicate that grapqhl path is appended with the request; also fixing potential nginx issue

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -33,7 +33,7 @@ import AdminPaths from './constants/AdminPaths';
 import './i18n';
 
 const client = new ApolloClient({
-  uri: `${process.env.REACT_APP_GRAPHQL_URL}/graphql`,
+  uri: `${process.env.REACT_APP_GRAPHQL_URL}`,
   cache: new InMemoryCache(),
 });
 
