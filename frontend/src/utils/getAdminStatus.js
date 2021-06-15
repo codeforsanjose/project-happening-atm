5e37fc3e53372106594e9a749c658b02660bcfc5
Patrick Pelayo <PatrickP@PatrickP.Tech>
2021-06-15 10:24:18 -0700

    fixed typo

--- frontend/src/utils/getAdminStatus.js
+++ frontend/src/utils/getAdminStatus.js
@@ -2,7 +2,7 @@ import parseJwt from './parseJwt';
 import LocalStorageTerms from '../constants/LocalStorageTerms';
 import UserRoles from '../constants/UserRoles';
 
-// returns returns true if the user is an admin
+// returns true if the user is an admin
 const getAdminStatus = () => {
   const token = localStorage.getItem(LocalStorageTerms.TOKEN);
   const parsedToken = parseJwt(token);
