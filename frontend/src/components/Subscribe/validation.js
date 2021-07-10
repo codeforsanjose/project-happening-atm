d244f95125fc982795d580c6ac0c7465ff8c6932
Anna Kartynnik <anna.kartynnik@gmail.com>
2021-07-10 15:01:43 -0700

    #230: added a mask and a note that we support only US phone numbers.

--- frontend/src/components/Subscribe/validation.js
+++ frontend/src/components/Subscribe/validation.js
@@ -1,5 +1,4 @@
 const REQUIRED_FIELD_ERROR = 'This field is required.';
-const NO_NUMERIC_PHONE = 'Number not numeric. Please use only digits in your phone number.';
 const WRONG_COUNTRY_CODE = 'Country code is required to be 1.';
 const UNEXPECTED_PHONE_LENGTH = 'Unexpected length of the phone. Country and area codes are required.';
 const BAD_EMAIL = 'We didn\u2019t recognize this email address.';
@@ -8,18 +7,19 @@ const NO_AT_IN_EMAIL = 'An email address must contain a single @.';
 // Taken from: https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
 const EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+export function getPhoneDigits(phone) {
+  return (phone.match(/\d/g) || []).join('');
+}
+
 export function validatePhone(phone) {
   if (!phone) {
     return REQUIRED_FIELD_ERROR;
   }
-  const isPhoneNumeric = /^\d+$/.test(phone);
-  if (!isPhoneNumeric) {
-    return NO_NUMERIC_PHONE;
-  }
-  if (!phone.startsWith('1')) {
+  const phoneDigits = getPhoneDigits(phone);
+  if (!phoneDigits.startsWith('1')) {
     return WRONG_COUNTRY_CODE;
   }
-  if (phone.length !== 11) {
+  if (phoneDigits.length !== 11) {
     return UNEXPECTED_PHONE_LENGTH;
   }
   return null;
