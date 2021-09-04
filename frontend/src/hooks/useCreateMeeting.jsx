7aa22bdfc19057d06789e9b58a80d6ea450c6c5b
Jerry Yip <jerry.yip@gmail.com>
2021-09-03 17:16:38 -0700

    Create custom hook for meeting creation

---
+++ frontend/src/hooks/useCreateMeeting.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable camelcase */
+import React, { useState } from 'react';
+import CreateMeetingModal from '../components/CreateMeetingModal/CreateMeetingModal';
+
+/**
+ * Used to implement meeting creation on a page.
+ *
+ * Returns:
+ *  openModal
+ *    Callback function to show the create meeting modal
+ *  CreateModal
+ *    Modal component
+ */
+
+ const useCreateMeeting = () => {
+    const [showModal, setShowModal] = useState(false);
+    const closeModal = () => { setShowModal(false); };
+    const openModal = () => { setShowModal(true); };
+  
+    const CreateModal = () => (
+      showModal
+        ? (
+          <CreateMeetingModal
+            isOpen={showModal}
+            closeModal={closeModal}
+          />
+        ) : null
+    );
+  
+    return [openModal, CreateModal];
+  };
+  
+  export default useCreateMeeting;
