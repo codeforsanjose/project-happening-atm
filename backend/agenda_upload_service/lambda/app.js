116519cafcb453083ded1e289921bcf97c349787
Jerry Yip <jerry.yip@gmail.com>
2021-09-30 19:34:17 -0700

    add CORs support for csv agenda upload

--- backend/agenda_upload_service/lambda/app.js
+++ backend/agenda_upload_service/lambda/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const upload = require('express-fileupload');
 const getRoutes = require('./routes');
+// CORs for allowing cross origin requests
+const cors = require('cors');
 
 module.exports = (logger) => {
   const app = express();
   const routes = getRoutes(logger);
+
+  // CORs for allowing cross origin requests
+  app.use(cors())
 
   app.use(upload());
   app.use(routes);
