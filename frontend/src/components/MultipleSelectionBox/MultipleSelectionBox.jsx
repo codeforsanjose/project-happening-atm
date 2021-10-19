c213183069fe81c67cc72c1d10499608e3dcbe62
Jerry Yip <jerry.yip@gmail.com>
2021-10-19 16:08:18 -0700

    apply linter code formatting

--- frontend/src/components/MultipleSelectionBox/MultipleSelectionBox.jsx
+++ frontend/src/components/MultipleSelectionBox/MultipleSelectionBox.jsx
@@ -28,80 +28,68 @@ import { buildSubscriptionQueryString, convertQueryStringToServerFormat } from '
 *      An error object returned from the server if there is any error
 */
 
-function MultipleSelectionBox({
-  selectedItems,
-  handleCancel,
-}) {
-  const { t } = useTranslation();
-  const history = useHistory();
-  const [subscriptions, setSubscriptions] = useState(null);
-  const [createSubscriptions, { loading, error }] = useMutation(
-    CREATE_SUBSCRIPTIONS,
-    {
-      onCompleted: (data) => setSubscriptions(data?.createSubscriptions ?? null),
-    },
-  );
-  
-  const getNumberOfSelectedMeetingItems = () => {
-    let counter = 0;
-    Object.keys(selectedItems).forEach((meetingId) => {
-      counter += Object.keys(selectedItems[meetingId]).length;
-    });
-    return counter;
-  };
+function MultipleSelectionBox({ selectedItems, handleCancel }) {
+	const { t } = useTranslation();
+	const history = useHistory();
+	const [ subscriptions, setSubscriptions ] = useState(null);
+	const [ createSubscriptions, { loading, error } ] = useMutation(CREATE_SUBSCRIPTIONS, {
+		onCompleted: (data) => setSubscriptions(data?.createSubscriptions ?? null),
+	});
 
-  const handleSubscribe = () => {
-    const phone = getUserPhone();
-    const email = getUserEmail();
-    const subscriptionQueryString = convertQueryStringToServerFormat(buildSubscriptionQueryString(selectedItems));
+	const getNumberOfSelectedMeetingItems = () => {
+		let counter = 0;
+		Object.keys(selectedItems).forEach((meetingId) => {
+			counter += Object.keys(selectedItems[meetingId]).length;
+		});
+		return counter;
+	};
 
-    createSubscriptions({
+	const handleSubscribe = () => {
+		const phone = getUserPhone();
+		const email = getUserEmail();
+		const subscriptionQueryString = convertQueryStringToServerFormat(buildSubscriptionQueryString(selectedItems));
+
+		createSubscriptions({
 			variables: {
 				phone_number: phone,
 				email_address: email,
 				meetings: subscriptionQueryString
 			}
 		});
-  };
-
-  const closeConfirmation = () => {
-		 history.go(0);
 	};
 
-  return (
-    <div className={classnames('multiple-selection-box')}>
-      <span className="bold">
-        {getNumberOfSelectedMeetingItems()}
-        &nbsp;
-        {t('standard.words.selected')}
-      </span>
-      {subscriptions &&
+	const closeConfirmation = () => {
+		history.go(0);
+	};
+
+	return (
+		<div className={classnames('multiple-selection-box')}>
+			<span className="bold">
+				{getNumberOfSelectedMeetingItems()}
+				&nbsp;
+				{t('standard.words.selected')}
+			</span>
+			{subscriptions &&
 			subscriptions.length > 0 && (
 				<SubscribeConfirmation numberOfSubscriptions={subscriptions.length} onClose={closeConfirmation} />
 			)}
 			{error && <div className="form-error">{error.message}</div>}
-      <span className="row action">
-        <NotificationsIcon width={16} />
-        {loading && <Spinner />}
-        <button type="button" onClick={handleSubscribe}>
-          {loading ? (
-								t('standard.buttons.subscribing')
-							) : (
-								t('standard.buttons.subscribe')
-							)}
-        </button>
-      </span>
-      <span className="row action">
-        <ShareIcon width={18} />
-        <button type="button">
-          {t('standard.buttons.share')}
-        </button>
-      </span>
-      <button type="button" onClick={handleCancel}>
-        {t('standard.buttons.cancel')}
-      </button>
-    </div>
-  );
+			<span className="row action">
+				<NotificationsIcon width={16} />
+				{loading && <Spinner />}
+				<button type="button" onClick={handleSubscribe}>
+					{loading ? t('standard.buttons.subscribing') : t('standard.buttons.subscribe')}
+				</button>
+			</span>
+			<span className="row action">
+				<ShareIcon width={18} />
+				<button type="button">{t('standard.buttons.share')}</button>
+			</span>
+			<button type="button" onClick={handleCancel}>
+				{t('standard.buttons.cancel')}
+			</button>
+		</div>
+	);
 }
 
 export default MultipleSelectionBox;
