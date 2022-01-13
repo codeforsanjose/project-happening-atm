231069836d1d8cfd3ece999215b0cd5b5c08f119
Patrick Pelayo <PatrickP@PatrickP.tech>
2022-01-13 14:42:24 -0800

    removing unnecessary changes

--- frontend/src/constants/OauthClientID.js
+++ frontend/src/constants/OauthClientID.js
@@ -1,7 +1,7 @@
 // Contains the client ids for Google and Microsoft
 
 const CLIENT_ID = {
-  GOOGLE: '820675102685-hpi24tubo4uk7fbjf47tioaehfjgr3ns.apps.googleusercontent.com',
+  GOOGLE: "",
 };
 
 export default CLIENT_ID;
