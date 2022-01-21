1ec12a28efdae4fb2a3faffbda115ff6493f95f5
Jerry Yip <jerry.yip@gmail.com>
2022-01-20 18:27:50 -0800

    remove Participant Tab for mtg agenda view

--- frontend/src/components/MeetingView/MeetingView.jsx
+++ frontend/src/components/MeetingView/MeetingView.jsx
@@ -1,17 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import { useTranslation } from 'react-i18next';
-import './MeetingView.scss';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import "./MeetingView.scss";
+import { useParams } from "react-router-dom";
 
-import { useLazyQuery, useQuery } from '@apollo/client';
-import { GET_MEETING_WITH_ITEMS } from '../../graphql/graphql';
+import { useLazyQuery, useQuery } from "@apollo/client";
+import { GET_MEETING_WITH_ITEMS } from "../../graphql/graphql";
 
-import NavBarHeader from '../NavBarHeader/NavBarHeader';
-import Header from '../Header/Header';
-import ParticipateView from './ParticipateView/ParticipateView';
-import AgendaView from './AgendaView/AgendaView';
-import Spinner from '../Spinner/Spinner';
-
+import NavBarHeader from "../NavBarHeader/NavBarHeader";
+import Header from "../Header/Header";
+//import ParticipateView from "./ParticipateView/ParticipateView";
+import AgendaView from "./AgendaView/AgendaView";
+import Spinner from "../Spinner/Spinner";
+import {
+  JoinMeetingIcon,
+} from '../../utils/_icons';
 /**
  * Component that displays a list of a meeting's agenda items.
  * Utilizes react-accessible-accordion to display groups of items.
@@ -19,8 +21,6 @@ import Spinner from '../Spinner/Spinner';
  * state:
  *    meetingWithItems
  *      An object representing the current meeting with an array of its agenda items
- *    showAgendaView
- *      A boolean value indicating if the Agenda or Participate View is shown
  *    navToggled
  *      A boolean value indicating if the header nav component is open
  */
@@ -37,22 +37,24 @@ function MeetingView() {
   // queries
   const { loading, error, data } = useQuery(GET_MEETING_WITH_ITEMS, {
     variables: { id: parseInt(id, 10) },
-    fetchPolicy: 'network-only',
+    fetchPolicy: "network-only",
   });
 
   // states
   const [meetingWithItems, setMeetingWithItems] = useState({});
-  const [showAgendaView, setShowAgendaView] = useState(true);
   const [navToggled, setNavToggled] = useState(false);
   // flag to indicate meeting items need to be saved
   const [saveMeetingItems, setSaveMeetingItems] = useState(false);
   // flag to indicate meeting items were updated by mutators
   const [meetingItemsUpdated, setMeetingItemsUpdated] = useState(false);
-  const [disableParticipateViewButton, setDisableParticipateViewButton] = useState(false);
+  // const [disableParticipateViewButton, setDisableParticipateViewButton] =
+  //   useState(false);
   // lazy queries
   const [getMeetingWithItems] = useLazyQuery(GET_MEETING_WITH_ITEMS, {
-    fetchPolicy: 'cache-and-network',
-    onCompleted: (d) => { createMeeting(d, setMeetingWithItems); },
+    fetchPolicy: "cache-and-network",
+    onCompleted: (d) => {
+      createMeeting(d, setMeetingWithItems);
+    },
   });
 
   function handleToggle() {
@@ -66,12 +68,12 @@ function MeetingView() {
     }
   }, [data]);
 
-  // disabled participate view while mutators are being processed by backend
-  useEffect(() => {
-    if (saveMeetingItems) {
-      setDisableParticipateViewButton(true);
-    }
-  }, [saveMeetingItems]);
+  // // disabled participate view while mutators are being processed by backend
+  // useEffect(() => {
+  //   if (saveMeetingItems) {
+  //     setDisableParticipateViewButton(true);
+  //   }
+  // }, [saveMeetingItems]);
 
   // once mutators finish fetch the new data
   useEffect(() => {
@@ -80,7 +82,7 @@ function MeetingView() {
         variables: { id: parseInt(id, 10) },
       });
       setMeetingItemsUpdated(false);
-      setDisableParticipateViewButton(false);
+      // setDisableParticipateViewButton(false);
     }
   }, [meetingItemsUpdated, id, getMeetingWithItems]);
 
@@ -91,43 +93,35 @@ function MeetingView() {
         loading={loading}
         meeting={meetingWithItems}
         setSaveMeetingItems={setSaveMeetingItems}
-        showUpdateStatus={showAgendaView}
       />
-
-      <div className="view-toggle">
-        <div className={showAgendaView ? 'view-active' : ''}>
-          <button
-            type="button"
-            onClick={() => setShowAgendaView(true)}
-          >
-            {t('meeting.tabs.agenda.label')}
-          </button>
-        </div>
-        <div className={showAgendaView ? '' : 'view-active'}>
-          <button
-            type="button"
-            onClick={() => setShowAgendaView(false)}
-            disabled={disableParticipateViewButton}
-          >
-            {t('meeting.tabs.participate.label')}
-          </button>
-        </div>
-      </div>
       {loading && <Spinner />}
-      {!(loading || error) && data && 'items' in meetingWithItems
-        && (
-          showAgendaView
-            ? (
-              <AgendaView
-                meeting={meetingWithItems}
-                saveMeetingItems={saveMeetingItems}
-                setSaveMeetingItems={setSaveMeetingItems}
-                setMeetingItemsUpdated={setMeetingItemsUpdated}
-              />
-            )
-            : <ParticipateView meeting={meetingWithItems} />
-        )}
-      {error && <p className="error">{ error.message }</p>}
+      {!(loading || error) && data && "items" in meetingWithItems && (
+        <a
+          meeting={meetingWithItems}
+          href={meetingWithItems.virtual_meeting_url}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button 
+          type="button"
+          className="join-meeting"
+          >
+          <JoinMeetingIcon/>          
+            <p>{t(
+              "meeting.tabs.participate.section.join.description.number-2.button"
+            )}</p>
+          </button>
+        </a>
+      )}
+      {!(loading || error) && data && "items" in meetingWithItems && (
+        <AgendaView
+          meeting={meetingWithItems}
+          saveMeetingItems={saveMeetingItems}
+          setSaveMeetingItems={setSaveMeetingItems}
+          setMeetingItemsUpdated={setMeetingItemsUpdated}
+        />
+      )}
+      {error && <p className="error">{error.message}</p>}
     </div>
   );
 }
