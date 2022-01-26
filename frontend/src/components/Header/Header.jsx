85fd9d495a676267518523ff71f04b2bd944c15a
Jerry Yip <jerry.yip@gmail.com>
2022-01-25 16:26:51 -0800

    fix linter formatting

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,27 +1,25 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
-import './Header.scss';
-import classnames from 'classnames';
-import Spinner from '../Spinner/Spinner';
+import React from "react";
+import { useTranslation } from "react-i18next";
+import "./Header.scss";
+import classnames from "classnames";
+import Spinner from "../Spinner/Spinner";
 import {
   toDateString,
   toTimeString,
   isFutureTimestamp,
   getDifference,
-} from '../../utils/timestampHelper';
+} from "../../utils/timestampHelper";
 
 // Asset imports
-import cityLogo from '../../assets/SanJoseCityLogo.png';
+import cityLogo from "../../assets/SanJoseCityLogo.png";
 
 // functions used by this component
-import isAdmin from '../../utils/isAdmin';
+import isAdmin from "../../utils/isAdmin";
 
-const MEETING_RELATIVE_TIME_LOC_KEY_PREFIX = 'meeting.status.relative.long.';
-const PAST_MEETING_STATUS_LOC_KEY = 'meeting.status.long.ended';
+const MEETING_RELATIVE_TIME_LOC_KEY_PREFIX = "meeting.status.relative.long.";
+const PAST_MEETING_STATUS_LOC_KEY = "meeting.status.long.ended";
 
-function Header({
-  loading, meeting, setSaveMeetingItems
-}) {
+function Header({ loading, meeting, setSaveMeetingItems }) {
   const { t } = useTranslation();
 
   const getRelativeTimeLocKey = () => {
@@ -31,56 +29,59 @@ function Header({
       if (diffInDays > 6) {
         return `${MEETING_RELATIVE_TIME_LOC_KEY_PREFIX}in-1-week`;
       }
-      return `${MEETING_RELATIVE_TIME_LOC_KEY_PREFIX}in-${diffInDays}-day${diffInDays <= 1 ? '' : 's'}`;
+      return `${MEETING_RELATIVE_TIME_LOC_KEY_PREFIX}in-${diffInDays}-day${
+        diffInDays <= 1 ? "" : "s"
+      }`;
     }
     return PAST_MEETING_STATUS_LOC_KEY;
   };
 
   return (
-    <div className={classnames('header')}>
-      <div className={classnames('header-content')}>
+    <div className={classnames("header")}>
+      <div className={classnames("header-content")}>
         <img className="logo" src={cityLogo} alt="logo" />
         <div className="meeting-info">
-          <div className="title">
-            {t('header.city-council-meeting-agenda')}
-          </div>
+          <div className="title">{t("header.city-council-meeting-agenda")}</div>
 
-          <div className="details-title">
-            Meeting Details
-          </div>
+          <div className="details-title">Meeting Details</div>
 
           {loading && <Spinner />}
 
           {!loading && (
             <>
               <div className="date">
-                {toDateString(meeting.meeting_start_timestamp, 'dddd, MMMM D, YYYY')}
+                {toDateString(
+                  meeting.meeting_start_timestamp,
+                  "dddd, MMMM D, YYYY"
+                )}
               </div>
 
               <div className="time">
                 {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
-                {t('meeting.start-time')}:
-                {' '}
-                <span className="no-bold">{toTimeString(meeting.meeting_start_timestamp)}</span>
+                {t("meeting.start-time")}:{" "}
+                <span className="no-bold">
+                  {toTimeString(meeting.meeting_start_timestamp)}
+                </span>
               </div>
 
-              <div className="status">
-                {t(getRelativeTimeLocKey())}
-              </div>
+              <div className="status">{t(getRelativeTimeLocKey())}</div>
 
-              {
-              isAdmin() && (
+              {isAdmin() && (
                 <>
                   <div className="saveStatus">
-                    {t('meeting.status.label')}
-                    :
-                    <button className="saveStatusButton" type="button" onClick={() => { setSaveMeetingItems(true); }}>
-                      {t('meeting.status.short.default-option')}
+                    {t("meeting.status.label")}:
+                    <button
+                      className="saveStatusButton"
+                      type="button"
+                      onClick={() => {
+                        setSaveMeetingItems(true);
+                      }}
+                    >
+                      {t("meeting.status.short.default-option")}
                     </button>
                   </div>
                 </>
-              )
-              }
+              )}
             </>
           )}
         </div>
