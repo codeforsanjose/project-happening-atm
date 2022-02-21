31e2707778cf6ee2d947c5d151644169a64a6188
Patrick Pelayo <PatrickP@PatrickP.tech>
2022-02-21 09:42:03 -0800

    now styling the confirmationModal

--- frontend/src/components/ChangeMeetingStatusModal/ChangeMeetingStatusModal.jsx
+++ frontend/src/components/ChangeMeetingStatusModal/ChangeMeetingStatusModal.jsx
@@ -129,7 +129,7 @@ const ChangeMeetingStatusModal = ({
         </div>
       </div>
       )}
-      {showItemStatusModal && <UpdateItemStatusModal />}
+      {showItemStatusModal && <UpdateItemStatusModal setShowItemStatusModal={setShowItemStatusModal} />}
     </Modal>
   );
 };
--- frontend/src/components/ChangeMeetingStatusModal/updateItemStatusModal/UpdateItemStatusModal.jsx
+++ frontend/src/components/ChangeMeetingStatusModal/updateItemStatusModal/UpdateItemStatusModal.jsx
@@ -3,25 +3,45 @@ import React, { useState, useEffect } from 'react';
 import Modal from 'react-modal/lib/components/Modal';
 import './UpdateItemStatusModal.scss';
 import { CloseIcon } from '../../../utils/_icons';
-
-const style = {
-  overlay: {
-    backgroundColor: 'none',
-  },
-};
+import MeetingItemStates from '../../../constants/MeetingItemStates';
 
 const UpdateItemStatusModal = (
 ) => {
+  const [buttonClasses] = useState({
+    completed: {
+      status: MeetingItemStates.COMPLETED,
+      class: 'completed',
+    },
+  });
+  const style = {
+    overlay: {
+      backgroundColor: 'none',
+    },
+  };
+
   Modal.setAppElement('#root');
 
   return (
     <Modal style={style} className="UpdateItemStatusModal" isOpen>
       <div className="updateItemStatusWrapper">
         <div className="updateItemStatusHeader">
-          <span>Update Item Status</span>
-          <CloseIcon />
+          <span className="headerText">Update Item Status</span>
+          <div className="closeOutModal">
+            <CloseIcon className="closeOutIcon" />
+          </div>
         </div>
-        <div>Body</div>
+        <div className="updateItemStatuModalBody">
+          <div className="buttonsAndArrow">
+            <input type="button" value="blah" />
+            <div>An Arrow</div>
+            <input type="button" value="button2" />
+            <p className="statusChangeDescription">Stuff Goes here</p>
+            <div className="publishOrCancelButtons">
+              <input type="button" className="publish" value="Publish" />
+              <input type="button" className="cancel" value="Cancel" />
+            </div>
+          </div>
+        </div>
       </div>
     </Modal>
   );
