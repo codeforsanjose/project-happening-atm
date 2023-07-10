c9d1baa2d4767e35474cd4b02332d98f19b10b92
Jerry Yip <jerry.yip@gmail.com>
2023-07-10 11:33:17 +0200

    fix typo for postRequest spelling

--- backend/graphql_api/graphql/services/authentication.js
+++ backend/graphql_api/graphql/services/authentication.js
@@ -1,11 +1,11 @@
-const { postRequst } = require("./fetchingWrapper");
+const { postRequest } = require("./fetchingWrapper");
 module.exports = async function loginUser(username, passphrase) {
   try {
     const body = {
       username,
       password: passphrase,
     };
-    const result = await postRequst("/auth/login", body);
+    const result = await postRequest("/auth/login", body);
     return result.data.user;
   } catch (e) {
     console.log("error in login", e.response.status);
