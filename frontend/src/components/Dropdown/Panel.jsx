2a52c70fd4f3b3543f94e91d2cf06ba1abe26382
Jerry Yip <jerry.yip@gmail.com>
2023-08-09 23:42:50 -0700

    convert dropdown panel to html button from div
    using html button instead of div will help with future efforts for a11y
    (accessibility) support

--- frontend/src/components/Dropdown/Panel.jsx
+++ frontend/src/components/Dropdown/Panel.jsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import classNames from 'classnames';
+import React from "react";
+import classNames from "classnames";
 
-import './Panel.scss';
+import "./Panel.scss";
 
 /*
  * re-usable dropdown panel to reduce duplicate styling etc.
  * props: children, className, ...rest
- *  children: children elements to passthrough and show inside of panel
- *    className: additional custom styling classes to merge with panel styling
- *    rest: any additional props to pass through
+ * 	children: children elements to passthrough and show inside of panel
+ *		className: additional custom styling classes to merge with panel styling
+ *		rest: any additional props to pass through
  */
 
 function Panel({ children, className, ...rest }) {
-  const mergedClassNames = classNames('panel', className);
+  const mergedClassNames = classNames("panel", className);
 
   return (
-    <div {...rest} className={mergedClassNames}>
+    <button type="button" {...rest} className={mergedClassNames}>
       {children}
-    </div>
+    </button>
   );
 }
 
