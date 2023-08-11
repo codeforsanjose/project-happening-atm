6a6c26836306bf13a84ed5ec22b5393f48c108f8
Jerry Yip <jerry.yip@gmail.com>
2023-08-11 14:17:17 -0700

    fix indent spacing due to linter config

--- frontend/src/components/Dropdown/Panel.jsx
+++ frontend/src/components/Dropdown/Panel.jsx
@@ -1,20 +1,24 @@
-import React from "react";
-import classNames from "classnames";
+import React from 'react';
+import classNames from 'classnames';
 
-import "./Panel.scss";
+import './Panel.scss';
 
-/* 
-* re-usable dropdown panel to reduce duplicate styling etc.
-* props: children, className, ...rest
-* 	children: children elements to passthrough and show inside of panel
-*		className: additional custom styling classes to merge with panel styling
-*		rest: any additional props to pass through
-*/
+/*
+ * re-usable dropdown panel to reduce duplicate styling etc.
+ * props: children, className, ...rest
+ *  children: children elements to passthrough and show inside of panel
+ *    className: additional custom styling classes to merge with panel styling
+ *    rest: any additional props to pass through
+ */
 
-function Panel({children, className, ...rest }){
-	const mergedClassNames = classNames('panel', className);
+function Panel({ children, className, ...rest }) {
+  const mergedClassNames = classNames('panel', className);
 
-	return <div {...rest} className={mergedClassNames}>{children}</div>
+  return (
+    <div {...rest} className={mergedClassNames}>
+      {children}
+    </div>
+  );
 }
 
 export default Panel;
