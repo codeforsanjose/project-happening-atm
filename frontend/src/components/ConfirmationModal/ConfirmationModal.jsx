3aac503b3b099d011367a5f63f50095e28e2a320
Jerry Yip <jerry.yip@gmail.com>
2023-09-01 14:05:15 -0700

    create confirmation modal component
    develop initial foundational code for a re-usable confirmation modal

---
+++ frontend/src/components/ConfirmationModal/ConfirmationModal.jsx
@@ -0,0 +1,76 @@
+import React, { useCallback, useState } from "react";
+import { useHistory } from "react-router-dom";
+import Modal from "react-modal";
+import { useTranslation } from "react-i18next";
+import { CloseIcon } from "../../utils/_icons";
+// import Spinner from "../Spinner/Spinner"; // handled by the action bar
+import "./ConfirmationModal.scss";
+import classNames from "classnames";
+
+/**
+ * A common re-usable modal for Create / Update / Delete actions
+ *
+ * props:
+ *    isOpen
+ *      Boolean state that indicates if the modal is shown
+ *    closeModal
+ *      Callback function to close the modal
+ * 		headerText
+ * 			text to display in the modal header
+ * 		bodyText
+ *    	text to display in the modal body
+ *		actionButton
+ *			confirmation button (for updating/creating/deleting)
+ *		cancelButton
+ *			cancel button
+ * 		className
+ * 			any custom styling classes for given confirmation modal's use case
+ */
+
+function ConfirmationModal({
+  isOpen,
+  closeModal,
+  headerText,
+  bodyText,
+  actionButton,
+  cancelButton,
+  className,
+}) {
+  const history = useHistory();
+  const { t } = useTranslation();
+
+  Modal.setAppElement("#root"); // enables react-modal to set aria-hidden = true for non-modal page content
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={closeModal}
+      className={classNames("confirmation-modal", className)}
+      overlayClassName="modal-overlay"
+    >
+      <div className="modal-header">
+        <h2>{t(headerText)}</h2>
+        <button
+          type="button"
+          className="close-modal"
+          onClick={closeModal}
+          aria-label={t("standard.buttons.close")}
+        >
+          <CloseIcon />
+        </button>
+      </div>
+      <div className={classNames("modal-body", className)}>
+        <p className="modal-body-text">{bodyText}</p>
+        {/* circle back to ability to:
+				(i) handle errors & loading
+				(ii) translation of labels, perhaps via button component w/ translation of children */}
+        <div className="modal-buttons">
+          {actionButton}
+          {cancelButton}
+        </div>
+      </div>
+    </Modal>
+  );
+}
+
+export default ConfirmationModal;
