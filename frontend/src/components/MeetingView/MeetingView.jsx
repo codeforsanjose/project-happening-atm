3251362ee6c689b753334939c66bf093ae55a936
Jerry Yip <jerry.yip@gmail.com>
2023-11-15 12:28:41 -0800

    Store default mtg zoom url as envmt var
    Store default mtg zoom url as envmt var along with styling fixes

--- frontend/src/components/MeetingView/MeetingView.jsx
+++ frontend/src/components/MeetingView/MeetingView.jsx
@@ -36,7 +36,6 @@ function MeetingView() {
 
   // queries
   const { loading, error, data, refetch } = useQuery(GET_MEETING_WITH_ITEMS, {
-  const { loading, error, data, refetch } = useQuery(GET_MEETING_WITH_ITEMS, {
     variables: { id: parseInt(id, 10) },
     fetchPolicy: 'network-only',
     onCompleted: (data) => {
@@ -111,7 +110,6 @@ function MeetingView() {
 
   const agendaItemsPDFLink = meetingWithItems.agenda_pdf_link
     ? meetingWithItems.agenda_pdf_link
-    : '';
     : '';
   const linktoPDFAgendaItems = !(loading || error) && data && (
     <a className="agend-pdf-link" href={agendaItemsPDFLink} target="_blank">
