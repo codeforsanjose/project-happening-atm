1334f8dff64d2afb3ad450eb1132572f4649b599
Joseph Richardson <richardson.joseph1990@gmail.com>
2024-02-20 15:52:06 -0800

    what an idot of a mess you have made joey

--- backend/agenda_upload_service/routes/loginRoute/loginRoute.js
+++ backend/agenda_upload_service/routes/loginRoute/loginRoute.js
@@ -1,32 +1,35 @@
-const { Router } = require("express");
-const dbClient = require("../../db/dbClient.js");
-const { comparePassword } = require("../../utilities/auth.js");
+const { Router } = require('express');
+const dbClient = require('../../db/dbClient.js');
+const { comparePassword } = require('../../utilities/auth.js');
 
 function loginRoute(logger) {
   const router = Router();
-  router.post("/login", async function (req, res) {
+  router.post('/login', async (req, res) => {
+    let user;
     const client = await dbClient(logger);
     const { username, password } = req.body;
     try {
       const dbUser = await client.getAccountByEmail(username);
       if (dbUser.rows.length === 0) {
-        console.log("Email does not match our records please sign up");
-        throw new Error("Email does not match our records please sign up");
+        console.log('Email does not match our records please sign up');
+        throw new Error('Email does not match our records please sign up');
       }
       const dbPassword = dbUser.rows[0].password;
       if (!dbPassword) {
-        console.log("Sign in with your Google or Microsoft account");
-        throw new Error("Sign in with your Google or Microsoft account");
+        console.log('Sign in with your Google or Microsoft account');
+        throw new Error('Sign in with your Google or Microsoft account');
       }
       const isAuthenticated = await comparePassword(password, dbPassword);
       if (!isAuthenticated) {
-        console.log("Email and Password do not match");
-        throw new Error("Email and Password do not match");
+        console.log('Email and Password do not match');
+        throw new Error('Email and Password do not match');
       }
+      // eslint-disable-next-line prefer-destructuring
       user = dbUser.rows[0];
     } catch (e) {
       console.log(`Cannot authenticate email and password: ${e}`);
       throw new Error(e);
+    // eslint-disable-next-line brace-style
     }
     // finally end db connection
     finally {
